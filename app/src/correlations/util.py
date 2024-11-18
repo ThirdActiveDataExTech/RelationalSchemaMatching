bf94975bc87fe52cb1bcfeea5cea4ade6248e1c0
uoise <uoise@wisenut.co.kr>
2024-11-18 16:49:35 +0900

    feat: util.py 추가

---
+++ app/src/correlations/util.py
@@ -0,0 +1,15 @@
+import time
+from functools import wraps
+
+
+def time_logger(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        duration = end_time - start_time
+        print(f"'{func.__name__}': {duration:.4f} seconds")
+        return result
+
+    return wrapper
