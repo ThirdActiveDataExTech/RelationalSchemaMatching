e73ad642b314b8537b1d597e14dce5073309d79e
uoise <uoise@wisenut.co.kr>
2024-11-18 16:49:06 +0900

    feat: enums 추가

--- app/src/correlations/enums.py
+++ app/src/correlations/enums.py
@@ -2,11 +2,15 @@ from enum import Enum
 
 
 class Strategy(str, Enum):
-    ONE_TO_ONE = 'one-to-one'
-    ONE_TO_MANY = 'one-to-many'
-    MANY_TO_ONE = 'many-to-one'
-    MANY_TO_MANY = 'many-to-many'
+    ONE_TO_ONE = "one-to-one"
+    ONE_TO_MANY = "one-to-many"
+    MANY_TO_ONE = "many-to-one"
+    MANY_TO_MANY = "many-to-many"
 
 
 class MatchingModel(str, Enum):
-    INITIAL = 'initial_model'
+    INITIAL = "initial_model"
+
+
+class Combination(str, Enum):
+    TRAIN = "train"
