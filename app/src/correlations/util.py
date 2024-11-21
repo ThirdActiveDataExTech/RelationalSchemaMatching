bb6bcf4698f0c929ffe714ab1848a88966d302fc
uoise <uoise@wisenut.co.kr>
2024-11-21 16:30:08 +0900

    docs: time_logger specify

--- app/src/correlations/util.py
+++ app/src/correlations/util.py
@@ -10,7 +10,7 @@ def time_logger(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         duration = end_time - start_time
-        logging.info(f"'{func.__name__}': {duration:.4f} seconds")
+        logging.info(f"TIME_LOGGER: '{func.__name__}': {duration:.4f} seconds")
         return result
 
     return wrapper
