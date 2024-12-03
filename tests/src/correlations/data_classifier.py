6644e2fdbb874f620a28e2d1091ca1d8579c7f38
uoise <uoise@wisenut.co.kr>
2024-12-03 11:15:58 +0900

    feat: is_date testcase - fix: test_classify_url to test_classify

--- tests/src/correlations/data_classifier.py
+++ tests/src/correlations/data_classifier.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.src.correlations.data_classifier import is_url, DataTypes, classify_data_type
+from app.src.correlations.data_classifier import is_url, DataTypes, classify_data_type, is_date
 from tests.src.testcase import TestCase
 
 URL_DICT = {
@@ -26,8 +26,25 @@ URL_DICT = {
     "http://www.example.com/path/to/page.php?id=123&user=john": True
 }
 
-VALID_URLS = [u for u, v in URL_DICT.items() if v is True]
-INVALID_URLS = [u for u, v in URL_DICT.items() if v is False]
+DATE_DICT = {
+    "2023年6月1日": True,
+    "2022年12月31日": True,
+    "2021年1月15日": True,
+    "2022-12-31": True,
+    "15/01/2021": True,
+    "不是日期": True,  # SUS
+    "31/12/2022": True,
+    "1/15/2021": True,
+    "date": False,
+    "1999-12-31": True,
+    "2031-01-01": True,
+    "2023-06-01": True,
+    "": False,
+    "1,2,3": True,  # SUS
+    "年月日": True,  # SUS
+    "日月年": True,  # SUS
+    "月日年": True,  # SUS
+}
 
 
 class ClassifierTestCase(unittest.TestCase):
@@ -37,12 +54,28 @@ class ClassifierTestCase(unittest.TestCase):
             actual = is_url([url])
             self.assertEqual(actual, valid, f"{url}: expected: {valid}, actual: {actual}")
 
-    def test_classify_url(self):
+    def test_is_date(self):
+        for date, valid in DATE_DICT.items():
+            actual = is_date([date])
+            self.assertEqual(actual, valid, f"{date}: expected: {valid}, actual: {actual}")
+
+    def test_classify(self):
+        valid_urls = [u for u, v in URL_DICT.items() if v is True]
+        invalid_urls = [u for u, v in URL_DICT.items() if v is False]
+
+        valid_dates = [u for u, v in DATE_DICT.items() if v is True]
+        invalid_dates = [u for u, v in DATE_DICT.items() if v is False]
+
         classify_url_test_cases = [
-            TestCase("Valid Url List", VALID_URLS, DataTypes.URL),
-            TestCase("Invalid Url List", INVALID_URLS, DataTypes.STRING),
-            TestCase("Valid Ratio 50% List", [VALID_URLS[0]] * 5 + [INVALID_URLS[0]] * 5, DataTypes.STRING),
-            TestCase("Valid Ratio 90% List", [VALID_URLS[0]] * 9 + [INVALID_URLS[0]] * 1, DataTypes.URL),
+            TestCase("Valid Url List", valid_urls, DataTypes.URL),
+            TestCase("Invalid Url List", invalid_urls, DataTypes.STRING),
+            TestCase("Url Valid Ratio 50% List", [valid_urls[0]] * 5 + [invalid_urls[0]] * 5, DataTypes.STRING),
+            TestCase("Url Valid Ratio 90% List", [valid_urls[0]] * 9 + [invalid_urls[0]] * 1, DataTypes.URL),
+
+            TestCase("Valid Date List", valid_dates, DataTypes.DATE),
+            TestCase("Invalid Date List", invalid_urls, DataTypes.STRING),
+            TestCase("Date Valid Ratio 50% List", [valid_dates[0]] * 5 + [invalid_dates[0]] * 5, DataTypes.STRING),
+            TestCase("Date Valid Ratio 90% List", [valid_dates[0]] * 9 + [invalid_dates[0]] * 1, DataTypes.DATE),
         ]
 
         for c in classify_url_test_cases:
