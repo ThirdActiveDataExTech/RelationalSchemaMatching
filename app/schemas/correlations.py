25e46971da4f296155fcea3d6c25620aa194e2a1
uoise1 <uoise@wisenut.co.kr>
2025-10-29 08:52:46 +0900

    REFACTOR: router body 대신 form 방식으로 변경

--- app/schemas/correlations.py
+++ app/schemas/correlations.py
@@ -7,41 +7,11 @@ pydantic BaseModel을 기본적으로 활용한다.
     - @model_validator(...)를 사용하여 모델 적용 전과 후에 확인할 로직을 작성한다.
     > 자세한 사항은 pydantic 공식 문서 확인
 """
-from typing import Optional
 
 from pydantic import BaseModel, Field
 
 from app.schemas.response import APIResponseModel
-from app.src.correlations.enums import Strategy
 from app.version import VERSION
-
-
-class SchemaMatchingRequestModel(BaseModel):
-    # 사용할 데이터
-    l_table: str = Field(..., description="`s3://{BUCKET}/{KEY}` 또는 로컬 파일 경로")
-    r_table: str = Field(..., description="`s3://{BUCKET}/{KEY}` 또는 로컬 파일 경로")
-
-    # 결과 반환 관련 파라미터
-    truth_json: Optional[str] = Field(description="l_table, r_table에 대한 Groun Truth. `s3://{BUCKET}/{KEY}` 또는 로컬 파일 경로",
-                                      default=None)
-
-    # 하이퍼파라미터
-    model: str = Field(description="model path", default="initial")
-    strategy: str = Field(description="strategy", default=str(Strategy.MANY_TO_MANY))
-    threshold: Optional[float] = Field(description="threshold", default=None)
-
-    # S3/MinIO 관련 파라미터 (모두 Optional로 설정)
-    endpoint_url: Optional[str] = Field(description="S3/MinIO endpoint url", default=None)
-    access_key: Optional[str] = Field(description="access key", default=None)
-    secret_key: Optional[str] = Field(description="secret key", default=None)
-    region_name: Optional[str] = Field(description="region name", default=None)
-
-
-class DatasetMatchingRequestModel(BaseModel):
-    dataset: str = Field(description="dataset path")
-    model: str = Field(description="model path", default="initial")
-    strategy: str = Field(description="strategy", default=str(Strategy.MANY_TO_MANY))
-    threshold: Optional[float] = Field(description="threshold", default=None)
 
 
 class DummyCorrelation(BaseModel):
